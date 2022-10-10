96830fbfd0ca086618bd513f9998e6ecf3d211f3
Oleg <oleg.krut.64@gmail.com>
2022-10-10 16:44:01 +0700

    git push

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,17 +1,3 @@
 from django.db import models
 
-print("Hello")
-print("Hello")
-print("Hello")
-print("Hello")
-print("Hello")
-print("Hello")
-print("Hello")
-print("Hello")
-print("Hello")
-a = 202
-b = 202.4
-print("Hello")
-print("Hello")
-print("Hello")
 # Create your models here.
