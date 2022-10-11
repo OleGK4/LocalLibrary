a6915268c7d31c061115b069390b2a0cb6583a70
Oleg <oleg.krut.64@gmail.com>
2022-10-11 14:13:07 +0700

    git push

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -77,7 +77,7 @@ class BookInstance(models.Model):
         String for representing the Model object
         """
         return '%s (%s)' % (self.id, self.book.title)
-sd = 0
+
 
 class Genre(models.Model):
     """
